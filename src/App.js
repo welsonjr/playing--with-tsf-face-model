f8a92f98e957eadf015751aecf4fbb2fe1c36ba1
Welson Jr <welsonjr@gmail.com>
2022-05-22 00:58:36 +0200

    Initial

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import { atom, useAtom } from 'jotai'
+import { useRef } from 'react';
+import {drawMouth} from './drawing-util';
+import './App.css';
+import '@tensorflow/tfjs-backend-webgl';
+
+
+
+const imageAtom = atom(null);
+const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+const detectorConfig = {
+  runtime: 'tfjs', //'mediapipe', // or 'tfjs'
+  solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+}
+
+
+const detectorAtom = atom(async (get) => await faceLandmarksDetection.createDetector(model, detectorConfig));
+
+function App() {
+  const drawArea = useRef(null);
+  const fileRef = useRef(null);
+  const imgRef = useRef(null);
+  const [image, setImage] = useAtom(imageAtom);
+  const [detector] = useAtom(detectorAtom);
+  
+  const handleFileChange = (e) => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      const fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        setImage(e.target.result);
+      };
+      fileReader.readAsDataURL(e.target.files[0]);
+    }
+  };
+
+  const detectFaces = async (e) => {
+    e.preventDefault();
+    if(detector && image){
+      const predictions = await detector.estimateFaces(imgRef.current);
+      drawArea.current.width = imgRef.current.width;
+      drawArea.current.height = imgRef.current.height;
+      const ctx = drawArea.current.getContext('2d');
+      
+      drawMouth(ctx, predictions);
+    }
+  }
+
+  return (
+    <div className="App">
+      <form>
+        <button className="pure-button pure-button-primary" onClick={e => { 
+          e.preventDefault();
+          fileRef.current.click();
+        }}
+        > Add image</button>
+        <button disabled={!image} className="pure-button pure-button-primary" onClick={detectFaces}>Detect</button>
+        <input type="file" name="file" accept="image/*" ref={fileRef} onChange={handleFileChange} hidden/>
+      </form>
+      <div className='flex'>
+        <canvas className='canvas' ref={drawArea}/>
+        <img className='image' src={image} alt="preview" ref={imgRef}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/drawing-util.js
@@ -0,0 +1,48 @@
+// Lower outer.
+const lowerLipsOuter = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+// Upper outer(excluding corners).
+const upperLipsOuter = [185, 40, 39, 37, 0, 267, 269, 270, 409];
+// Lower inner.
+const lowerLipsInner = [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308];
+// Upper inner(excluding corners).
+const upperLipsInner = [191, 80, 81, 82, 13, 312, 311, 310, 415];
+// Lower semi - outer.
+const lowerLipsSemiOuter = [76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306];
+// Upper semi - outer(excluding corners).
+const upperLipsSemiOuter = [184, 74, 73, 72, 11, 302, 303, 304, 408];
+// Lower semi - inner.
+const lowerLipsSemiInner = [62, 96, 89, 179, 86, 15, 316, 403, 319, 325, 292];
+// Upper semi - inner(excluding corners).
+const upperLipsSemiInner = [183, 42, 41, 38, 12, 268, 271, 272, 407];
+
+const lowerLips = [
+  ...lowerLipsInner,
+  ...lowerLipsOuter,
+  ...lowerLipsSemiInner,
+  ...lowerLipsSemiOuter,
+];
+
+const upperLips = [
+  ...upperLipsInner,
+  ...upperLipsOuter,
+  ...upperLipsSemiInner,
+  ...upperLipsSemiOuter,
+];
+
+export const drawMouth = (ctx, predictions) => {
+  for (let f = 0; f < predictions.length; f++) {
+    const keypoints = predictions[f].keypoints;
+
+    drawMouthPoints(ctx, keypoints);
+  }
+};
+
+const drawMouthPoints = (ctx, keypoints) => {
+  const lips = [...lowerLips, ...upperLips].map((i) => keypoints[i]);
+  for (let i = 0; i < lips.length; i++) {
+    ctx.beginPath();
+    ctx.arc(lips[i].x, lips[i].y, 1 /* radius */, 0, 2 * Math.PI);
+    ctx.fillStyle = "aqua";
+    ctx.fill();
+  }
+};
